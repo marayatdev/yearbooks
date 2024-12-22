e6483374f7dc5eeca00a629030cd495d9040b257
marayatdev <149398037+marayatdev@users.noreply.github.com>
2024-12-22 20:09:05 +0700

    .

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Project</title>
+    <link href="/yearbooks/dist/css/style.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-red-100 p-10">
+    <h1 class="text-3xl text-blue-500 font-bold underline">
+        Hello world!
+    </h1>
+</body>
+
+</html>
